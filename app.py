04c3dd62fd1f7deca7c8d12d9592abeceb91635c
Justin Boggs <jboggs1@gmail.com>
2020-02-22 19:10:28 -0600

    updates

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-app.py
+#app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my 'Home' page."
+
+@app.route("/api/v1.0/precipitation")
+def precip():
+    print("Server received request for 'Precip' page...")
+    return "Welcome to my 'Precip' page."
+
+@app.route("/api/v1.0/stations")
+def stations():
+    print("Server received request for 'Stations' page...")
+    return "Welcome to my 'Stations' page."
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    print("Server received request for 'TOBS' page...")
+    return "Welcome to my 'TOBS' page."
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
